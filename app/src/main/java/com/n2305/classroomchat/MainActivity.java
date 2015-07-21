02b03db31705b7286cea40eb2ec7089629897da4
peaceman <nixalio@gmail.com>
2015-07-21 16:14:15 +0200

    websocket basics

--- app/src/main/java/com/n2305/classroomchat/MainActivity.java
+++ app/src/main/java/com/n2305/classroomchat/MainActivity.java
@@ -1,24 +1,39 @@
 package com.n2305.classroomchat;
 
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.drafts.Draft_76;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openWebSocket();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return false;
     }
 
     @Override
@@ -34,5 +49,48 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://peacedesk.n:1338/chat");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri, new Draft_17()) {
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                Log.i("WebSocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String message) {
+                final String msg = message;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView textView = (TextView)findViewById(R.id.messages);
+                        textView.setText(textView.getText() + "\n" + msg);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+                Log.i("WebSocket", "Closed " + reason);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("WebSocket", "Error " + e.getMessage());
+            }
+        };
+
+        mWebSocketClient.connect();
+        Log.i("WebSocket", "Started connection thread");
     }
 }
